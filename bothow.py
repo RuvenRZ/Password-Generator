92e2e811b61acd496b835f60d9e228e10faec6ea
RuvenRZ <ruven.reznikov@gmail.com>
2024-11-21 18:47:43 +0200

    Update and rename paswrdgnrtr.py to bothow.py

---
+++ bothow.py
@@ -0,0 +1,40 @@
+import telebot
+    
+    # Инициализация бота с использованием его токена
+bot = telebot.TeleBot("7468396677:AAH6slPbmoclJqZBHoQb_H7yq--UmzK5o1k")
+    
+    # Обработчик команды '/start' и '/hello'
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
+    print(message.chat.id)
+# Обработчик команды '/heh'
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+
+# Запуск бота
+
+GROUP_CHAT_ID = '-1002052649891'
+
+
+def is_api_group(chat_id):
+    return chat_id == GROUP_CHAT_ID
+
+@bot.message_handler(func=lambda m: True, content_types=['new_chat_participant'])
+def on_user_joins(message):
+    if not is_api_group(message.chat.id):
+        return
+
+    name = message.new_chat_participant.first_name
+    if hasattr(message.new_chat_participant, 'last_name') and message.new_chat_participant.last_name is not None:
+        name += u" {}".format(message.new_chat_participant.last_name)
+
+    if hasattr(message.new_chat_participant, 'username') and message.new_chat_participant.username is not None:
+        name += u" (@{})".format(message.new_chat_participant.username)
+
+    bot.reply_to(message, f'welcome {name}')
+
+
+bot.polling()
